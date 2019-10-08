74ae9c7128994df4a6ca3736fdfffd709af5410f
uchu-biosensors <noah@uchubiosensors.com>
2019-10-08 12:38:43 -0400

    tune timing

--- serialcomm.py
+++ serialcomm.py
@@ -8,6 +8,7 @@ baudrate = 9600
 filepath = "../CSV_files/"
 
 ser = serial.Serial(port, baudrate)
+has_started = False
 
 def receive():
     data = ser.read_until()
@@ -30,21 +31,26 @@ try:
         # CSV header titles for importing to Excel
         f.write("Time (ms),Vsd (ADC),V+ (ADC),Vout (ADC),Isd (ADC),Temp (ADC),Gnd (ADC),")
         f.write("Vsd (mV),V+ (mV),Vout (mV),Isd (mV),Temp (mV),Gnd (mV)\n")
-        start_time = int(round(time.time() * 1000))
         # Accept user input to pass to Arduino if appropriate, if not append data to CSV
         while True:
             ard_msg = receive();
             ard_msg_decoded = ard_msg.decode('ascii')
-            print(ard_msg_decoded), 
             if ard_msg_decoded[:1] == 'E':
+                print(ard_msg_decoded), 
                 res = raw_input()
                 transmit(res)
             elif ard_msg_decoded[:1] == 'W':
+                print(ard_msg_decoded), 
                 res = raw_input()
                 transmit(res)
             else:
+                if (has_started != True):
+                    has_started = True
+                    start_time = round(float(time.time()), 3)
+                # Add time to beginning of string
                 ard_msg_decoded = ',' + ard_msg_decoded
-                ard_msg_decoded = str((int(round(time.time() * 1000))) - start_time) + ard_msg_decoded
+                ard_msg_decoded = str(round(float(time.time() - start_time), 3)) + ard_msg_decoded
+                print(ard_msg_decoded), 
                 f.write(ard_msg_decoded)
 # Stop program, tell Arduino to "stop", and exit program on Ctrl + C
 except KeyboardInterrupt:
